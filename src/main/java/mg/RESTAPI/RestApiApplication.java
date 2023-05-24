1833b12f534bab4ae2369a5737c67e5df1006f33
mgace <matejgace@gmail.com>
2023-05-24 10:12:06 +0200

    Blog Appp on AWS Cloud

--- src/main/java/mg/RESTAPI/RestApiApplication.java
+++ src/main/java/mg/RESTAPI/RestApiApplication.java
@@ -5,7 +5,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import mg.RESTAPI.entity.Role;
+import mg.RESTAPI.repositories.RoleRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -29,7 +33,7 @@ import org.springframework.context.annotation.Bean;
 				url = "https://github.com/matejgace/REST-API"
 		)
 )
-public class RestApiApplication {
+public class RestApiApplication implements CommandLineRunner {
 
 	@Bean
 	public ModelMapper modelMapper(){
@@ -39,4 +43,18 @@ public class RestApiApplication {
 		SpringApplication.run(RestApiApplication.class, args);
 	}
 
+	@Autowired
+	private RoleRepository roleRepository;
+	@Override
+	public void run(String... args) throws Exception {
+
+		Role adminRole = new Role();
+		adminRole.setName("ROLE_ADMIN");
+		roleRepository.save(adminRole);
+
+		Role userRole = new Role();
+		userRole.setName("ROLE_USER");
+		roleRepository.save(userRole);
+
+	}
 }
